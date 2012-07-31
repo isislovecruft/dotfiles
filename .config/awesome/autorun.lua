8151f913222b373dd6a8ca359737fb6ef15b8e9f
Isis Lovecruft <isis@patternsinthevoid.net>
2012-07-31 03:35:06 +0000

    trying autorun.lua for some of the xinits

--- .config/awesome/autorun.lua
+++ .config/awesome/autorun.lua
@@ -1,11 +1,12 @@
 --run_once("xscreensaver", "-no-splash")         -- starts screensaver daemon 
---run_once("xsetroot", "-cursor_name left_ptr")  -- sets the cursor icon
-
 --run_once("redshift", "-o -l 0:0 -b 0.5 -t 6500:6500") -- brightness
 --run_once("ibus-daemon", "--xim") -- ibus
 --run_once(os.getenv("HOME") .. "/.dropbox-dist/dropboxd") -- dropbox
 --run_once("nm-applet") -- networking
-
 --run_once("wmname", "LG3D") -- java fix
-
 --run_once("sh " .. os.getenv("HOME") .. "/.screenlayout/dual-monitor.sh") -- set screens up
+
+run_once("xsetroot", "-cursor_name top_left_arrow")
+run_once("xset", "s off")
+run_once("xset", "dpms 300 600 600")
+run_once("xrdb", "-load " .. os.getenv("HOME") .. "/.Xdefaults")
